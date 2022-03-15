b6fa58ef2d6bb1ab658aab228f45bd3eaf9e5fad
davidformation <75276692+davidformation@users.noreply.github.com>
2022-03-15 12:01:14 -0700

    Remove the title from the markdown preview

--- init.el
+++ init.el
@@ -319,5 +319,5 @@ Version 2016-04-04"
 
 (defun markdown-html (buffer)
   (princ (with-current-buffer buffer
-    (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
+    (format "<!DOCTYPE html><html><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
   (current-buffer)))
