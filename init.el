5fd283f7e434b7f6ec1f64c9a1b82beed809193b
davidformation <75276692+davidformation@users.noreply.github.com>
2022-03-21 18:17:27 -0700

    Dired updates

--- init.el
+++ init.el
@@ -185,7 +185,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company-terraform grip-mode exec-path-from-shell pandoc markdownfmt markdown-preview-mode impatient-mode jedi lsp-python-ms json-mode org-tree-slide magit-gh-pulls terraform-mode graphql-mode org-superstar yaml-mode move-text org-bullets multiple-cursors company yasnippet go-mode zenburn-theme which-key use-package rainbow-delimiters magit lsp-ui lsp-ivy helpful doom-themes doom-modeline counsel-projectile command-log-mode all-the-icons-ivy-rich)))
+   '(dired-subtree company-terraform grip-mode exec-path-from-shell pandoc markdownfmt markdown-preview-mode impatient-mode jedi lsp-python-ms json-mode org-tree-slide magit-gh-pulls terraform-mode graphql-mode org-superstar yaml-mode move-text org-bullets multiple-cursors company yasnippet go-mode zenburn-theme which-key use-package rainbow-delimiters magit lsp-ui lsp-ivy helpful doom-themes doom-modeline counsel-projectile command-log-mode all-the-icons-ivy-rich)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -322,4 +322,32 @@ Version 2016-04-04"
 (defun markdown-html (buffer)
   (princ (with-current-buffer buffer
     (format "<!DOCTYPE html><html><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
-  (current-buffer)))
+	 (current-buffer)))
+
+;; File Management
+(setq insert-directory-program "gls" dired-use-ls-dired t)
+;; Dired
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind(("C-x C-j" . dired-jump))
+  :custom((dired-listing-switches "-lagho --group-directories-first"))
+  :config
+  (bind-key "C-p" #'dired-up-directory dired-mode-map)
+  (bind-key "C-n" #'dired-find-file dired-mode-map)
+  (bind-key "M-n" #'dired-find-file-other-window dired-mode-map)
+)
+
+(use-package dired-subtree :ensure t
+  :after dired
+  :config
+  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
+  (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
+
+;; (require 'ls-lisp)
+;; (setq ls-lisp-dirs-first t)
+;; (setq ls-lisp-use-insert-directory-program nil)
+
+(when (string= system-type "darwin")
+  (setq dired-use-ls-dired nil))
