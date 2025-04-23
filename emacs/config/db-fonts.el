ae70b0ab7b12386a5c67d3f1345b627141b66453
davidhbaek <75276692+davidhbaek@users.noreply.github.com>
2025-04-23 12:26:32 -0700

    Use the JetBrains Mono font in emacs

---
+++ emacs/config/db-fonts.el
@@ -0,0 +1,10 @@
+;;; db-fonts.el --- Font stuff
+
+;; Set JetBrains Mono as the default font
+(set-face-attribute 'default nil
+                    :family "JetBrains Mono"
+                    :height 150
+                    :weight 'normal)
+
+(provide 'db-fonts)
+;;; db-fonts.el ends here
