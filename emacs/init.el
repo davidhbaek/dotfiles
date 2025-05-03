881eb01d9ffe8223486979b217a3ce5dc7daae93
davidhbaek <75276692+davidhbaek@users.noreply.github.com>
2025-05-03 14:00:02 -0700

    Fix font loading

--- emacs/init.el
+++ emacs/init.el
@@ -35,7 +35,7 @@
  ;; If there is more than one, they won't work right.
  '(markdown-command "/usr/local/bin/pandoc")
  '(package-selected-packages
-   '(lua-mode terraform-mode python-isort web-mode markdown-ts-mode 0blayout 0x0 markdown-mode markdown-preview-eww markdown-preview-mode grip-mode treesit-auto lsp-pyright org-roam-ui org-jira esup gptel zuul zenburn-theme yasnippet yaml-mode which-key typescript-mode tree-sitter-langs todotxt tide rjsx-mode rainbow-delimiters python-mode python-black prettier-js ox-hugo org-tree-slide org-superstar org-roam org-bullets multiple-cursors move-text magit lsp-ui jtsx json-mode jedi ivy-prescient helpful graphql-mode go-mode flycheck-golangci-lint exec-path-from-shell editorconfig dotenv-mode doom-themes doom-modeline dockerfile-mode dired-subtree counsel-projectile company command-log-mode anzu all-the-icons-ivy-rich)))
+   '(smartparens lua-mode terraform-mode python-isort web-mode markdown-ts-mode 0blayout 0x0 markdown-mode markdown-preview-eww markdown-preview-mode grip-mode treesit-auto lsp-pyright org-roam-ui org-jira esup gptel zuul zenburn-theme yasnippet yaml-mode which-key typescript-mode tree-sitter-langs todotxt tide rjsx-mode rainbow-delimiters python-mode python-black prettier-js ox-hugo org-tree-slide org-superstar org-roam org-bullets multiple-cursors move-text magit lsp-ui jtsx json-mode jedi ivy-prescient helpful graphql-mode go-mode flycheck-golangci-lint exec-path-from-shell editorconfig dotenv-mode doom-themes doom-modeline dockerfile-mode dired-subtree counsel-projectile company command-log-mode anzu all-the-icons-ivy-rich)))
 
 ;; Set up the tree-sitter load path
 (setq treesit-extra-load-path
@@ -47,3 +47,27 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 160 :width normal :foundry "nil" :family "Menlo"))))
+ '(fixed-pitch ((t (:family "Monospace"))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-checkbox ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :font "Lucida Grande" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :font "Lucida Grande" :height 1.5))))
+ '(org-level-2 ((t (:inherit default :font "Lucida Grande" :height 1.3))))
+ '(org-level-3 ((t (:inherit default :font "Lucida Grande" :height 1.1))))
+ '(org-level-4 ((t (:inherit default :font "Lucida Grande"))))
+ '(org-level-5 ((t (:inherit default :font "Lucida Grande"))))
+ '(org-level-6 ((t (:inherit default :font "Lucida Grande"))))
+ '(org-level-7 ((t (:inherit default :font "Lucida Grande"))))
+ '(org-level-8 ((t (:inherit default :font "Lucida Grande"))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(variable-pitch ((t (:family "DejaVu Sans Mono")))))
