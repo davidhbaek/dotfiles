f4f49e7953c30a05945e16654ba4745486eb1df3
davidhbaek <75276692+davidhbaek@users.noreply.github.com>
2025-05-14 20:41:11 -0700

    ADd some llm chat functionality to emacs

--- emacs/config/db-fonts.el
+++ emacs/config/db-fonts.el
@@ -1,10 +1,30 @@
 ;;; db-fonts.el --- Font stuff
 
+;;; Commentary:
+;; Configuration for fonts in Emacs
+
+;;; Code:
+
 ;; Set JetBrains Mono as the default font
 (set-face-attribute 'default nil
                     :family "JetBrains Mono"
                     :height 150
                     :weight 'normal)
+
+;; Ensure Org mode uses JetBrains Mono for fixed-pitch
+(set-face-attribute 'fixed-pitch nil :family "JetBrains Mono")
+
+;; Force the default font configuration to take precedence
+;; This is run at a hook to ensure it happens after other configuration
+(defun db/enforce-font-settings ()
+  "Ensure font settings are applied correctly at startup."
+  (set-face-attribute 'default nil
+                      :family "JetBrains Mono"
+                      :height 150
+                      :weight 'normal))
+
+;; Add to startup hook to ensure fonts are set correctly
+(add-hook 'after-init-hook #'db/enforce-font-settings)
 
 (provide 'db-fonts)
 ;;; db-fonts.el ends here
--- emacs/init.el
+++ emacs/init.el
@@ -9,10 +9,15 @@
 
 ;;; Code:
 
+;; Move custom variables and faces to a separate file
+(setq custom-file (expand-file-name "custom.el" (file-name-directory load-file-name)))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 ;; Load configuration files
 (add-to-list 'load-path (expand-file-name "config" (file-name-directory load-file-name)))
 
+;; Load UI and package configurations first
 (require 'db-ui)
 (require 'db-packages)
 (require 'db-navigation)
@@ -26,16 +31,9 @@
 (require 'db-env)
 (require 'db-jira)
 (require 'db-terminal)
+(require 'db-copilot)
 (require 'db-fonts)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(markdown-command "/usr/local/bin/pandoc")
- '(package-selected-packages
-   '(smartparens lua-mode terraform-mode python-isort web-mode markdown-ts-mode 0blayout 0x0 markdown-mode markdown-preview-eww markdown-preview-mode grip-mode treesit-auto lsp-pyright org-roam-ui org-jira esup gptel zuul zenburn-theme yasnippet yaml-mode which-key typescript-mode tree-sitter-langs todotxt tide rjsx-mode rainbow-delimiters python-mode python-black prettier-js ox-hugo org-tree-slide org-superstar org-roam org-bullets multiple-cursors move-text magit lsp-ui jtsx json-mode jedi ivy-prescient helpful graphql-mode go-mode flycheck-golangci-lint exec-path-from-shell editorconfig dotenv-mode doom-themes doom-modeline dockerfile-mode dired-subtree counsel-projectile company command-log-mode anzu all-the-icons-ivy-rich)))
+(require 'db-gptel)
 
 ;; Set up the tree-sitter load path
 (setq treesit-extra-load-path
@@ -47,27 +45,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 160 :width normal :foundry "nil" :family "Menlo"))))
- '(fixed-pitch ((t (:family "Monospace"))))
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-checkbox ((t (:inherit fixed-pitch))))
- '(org-code ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-title ((t (:inherit default :font "Lucida Grande" :height 2.0 :underline nil))))
- '(org-level-1 ((t (:inherit default :font "Lucida Grande" :height 1.5))))
- '(org-level-2 ((t (:inherit default :font "Lucida Grande" :height 1.3))))
- '(org-level-3 ((t (:inherit default :font "Lucida Grande" :height 1.1))))
- '(org-level-4 ((t (:inherit default :font "Lucida Grande"))))
- '(org-level-5 ((t (:inherit default :font "Lucida Grande"))))
- '(org-level-6 ((t (:inherit default :font "Lucida Grande"))))
- '(org-level-7 ((t (:inherit default :font "Lucida Grande"))))
- '(org-level-8 ((t (:inherit default :font "Lucida Grande"))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
- '(variable-pitch ((t (:family "DejaVu Sans Mono")))))
